472bb6667363e04cf299611233c02f234b9be1b6
getabear <1874178998@qq.com>
2019-11-30 20:41:02 +0800

    测试文件

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import youdao
 import analytical_pdf
 a=youdao.youdao()
 path="./web.pdf"
-# path="D:/360Downloads/嵌入式开发实习 - 周军 - 应届生 - 17132302080.pdf"
+# path="D:/360Downloads/.pdf"
 analytical_pdf.parse(path,"./2.txt")
 with open ("./2.txt","r",encoding='utf-8') as f:
     text=f.read()
